36dba1a8403eb5659aebdbc7a42b15b040a92b8a
madrian lardprunk <harrychenmmv@gmail.com>
2022-03-08 22:37:23 +0000

    a

---
+++ assets/js/flag.js
@@ -0,0 +1,3 @@
+// :)
+var flags = ["n07_f0und", "th3_fl4g_15_1n_4n0th3r_c4s7l3", "n0_c1g4r", "unf0rtun4t3", "0ur_w38s173_1t5_8r0k3n", "7urn_17_0n_4nd_0ff_4g41n", "m1553d_4_s3m1c0l0n"];
+document.title = $(".article-title") = `x-c-3 | CTF{${flags[Math.floor(Math.random() * flags.length)]}}`;
