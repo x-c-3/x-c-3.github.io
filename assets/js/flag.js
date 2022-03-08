ad3f06cadb42061af1ad5b30a616ec4e95e7f09e
madrian lardprunk <harrychenmmv@gmail.com>
2022-03-08 22:48:36 +0000

    Update flag.js

--- assets/js/flag.js
+++ assets/js/flag.js
@@ -1,3 +1,7 @@
 // :)
-var flags = ["n07_f0und", "th3_fl4g_15_1n_4n0th3r_c4s7l3", "n0_c1g4r", "unf0rtun4t3", "0ur_w38s173_1t5_8r0k3n", "7urn_17_0n_4nd_0ff_4g41n", "m1553d_4_s3m1c0l0n"];
-document.title = $(".article-title") = `x-c-3 | CTF{${flags[Math.floor(Math.random() * flags.length)]}}`;
+(()=>{
+	var flags = ["n07_f0und", "uh_0h", "n0_c1g4r", "unf0r7un473", "r4m_requ1r3d", ":(", "m1553d_4_s3m1c0l0n", "n07_g00d", "*******"];
+	flag = `CTF{${flags[Math.floor(Math.random() * flags.length)]}}`;
+	$(".article-title")[0].innerText = flag;
+	document.title = `x-c-3 | ${flag}`;
+})();
